671f6e4841190906aab06b658f8b332ae134f2ff
vos6434 <kacper11122003@wp.pl>
2024-06-24 12:05:21 +0100

    AI Navigation
    Add AI Navigation package
    Add basic Networked NPC
    Press F to switch NPC destination

---
+++ Assets/Scripts/MultiplayerAgent.cs
@@ -0,0 +1,43 @@
+using FishNet.Object;
+using FishNet.Connection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MultiplayerAgent : NetworkBehaviour
+{
+    [SerializeField] private List<Transform> positions = new List<Transform>();
+
+    private NavMeshAgent agent;
+
+    private int positionIndex = 0;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            NextPosition();
+    }
+
+    [ServerRpc (RequireOwnership = false)]
+    void NextPosition()
+    {
+        positionIndex++;
+        if (positionIndex >= positions.Count)
+            positionIndex = 0;
+
+        agent.SetDestination(positions[positionIndex].position);
+    }
+}
