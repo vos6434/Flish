6d476f1d33c59b14f50a1546a0bec32f4484a481
vos6434 <kacper11122003@wp.pl>
2024-09-05 22:03:26 +0100

    Fix player drag

--- Assets/Scripts/RestartSceneTrigger.cs
+++ Assets/Scripts/RestartSceneTrigger.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using Unity.Netcode;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RestartSceneTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.GetComponent<PlayerController>().transform.position = new Vector3(0, 2, 0);
     }
 }
